9a0800e6d5c3af03c99f04f66e30418f4cb5b9c8
hyohyogo@yahoo.co.jp <hyohyogo@yahoo.co.jp>
2018-02-05 21:13:05 +0900

    added new comers page to sidebar

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -1,6 +1,12 @@
 <div id="sidebarContainer">
     <div id="sidebar" hidden>
-        <?php $pageList = ['TOP', 'ABOUT US', 'STAGES', 'CONTACT'];?>
+        <?php
+            if(get_post_status(224) == 'publish'){
+                $pageList = ['TOP', 'ABOUT US', 'STAGES', 'JOIN US', 'CONTACT'];
+            } else {
+                $pageList = ['TOP', 'ABOUT US', 'STAGES', 'CONTACT'];
+            }
+        ?>
         <?php foreach($pageList as $key => $page):?>
             <!-- <div class="sidebarElem" onclick="scrollToPage(<?php //echo json_encode($key);?>)"> -->
             <div class="sidebarElem" onclick="scrollToPage(<?php echo $key;?>)">
