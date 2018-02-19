60ad2384f179a4150584fc0eb3cbc5d217ffa9b5
hyohyogo@yahoo.co.jp <hyohyogo@yahoo.co.jp>
2018-02-19 13:57:12 +0900

    changed layout of new comers

--- pages/newComers.php
+++ pages/newComers.php
@@ -1,26 +1,8 @@
 <div id="newComersContainer">
-    <div id="newComersBills" class="flexslider">
-        <ul class="slides">
-            <li>
-                <img src="<?php echo get_template_directory_uri();?>/images/newComers/bill_front.png">
-            </li>
-            <li>
-                <img src="<?php echo get_template_directory_uri();?>/images/newComers/bill_back.png">
-            </li>
-        </ul>
-    </div>
-    <div id="newComersDesc">
-        <div style="font-size: 1.5em; padding-bottom: 10px; font-weight: bold">
-            JOIN US
-        </div>
-        <?php
-            // has to fix post number
-            $post = get_post(224);
-            $post_content = sanitize_post_field('post_content', $post->post_content, $post->ID, 'display');
-            echo $post_content;
-        ?>
-        <img id="sakura1" src="<?php echo get_template_directory_uri();?>/images/newComers/sakura.png">
-        <img id="sakura2" src="<?php echo get_template_directory_uri();?>/images/newComers/sakura.png">
+    <h1 style="position: absolute; top: 3%; left: 7%; color: white;">new comers</h1>
+    <div id="newComersBills">
+        <img id="newComersBill1" src="<?php echo get_template_directory_uri();?>/images/newComers/bill_front.png">
+        <img id="newComersBill2" src="<?php echo get_template_directory_uri();?>/images/newComers/bill_back.png">
     </div>
 </div>
 
